36f5e089eab5234d0096375375229bdd3d3f136d
seebaasiman <seebaasiman@gmail.com>
2025-01-14 11:57:35 -0300

    Updates

--- assets/DesktopHorizontal-8fe77a7c.js
+++
@@ -1,9 +0,0 @@
-import{r as o,w as r,a as n,j as e,x as t}from"./index-2235f59c.js";import l from"./index-81cf79c9.js";import i from"./index-8325ce4b.js";import"./SectionsProjects-03547a1c.js";const x=()=>{const a=o.useRef();return r(a,2),n(p,{ref:a,children:[e(s,{className:"panel",children:e(l,{})}),e(s,{className:"panel",children:e(i,{})})]})},p=t.section`
-  width: 200%;
-  height: 100%;
-  display: flex;
-  flex-wrap: wrap;
-`,s=t.span`
-  width: 100vw;
-  height: 100vh;
-`;export{x as default};
